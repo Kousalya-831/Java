60ebca91250b9d0a8d43a658bcaf050b55b0d757
Kousalya-831 <88423513+Kousalya-831@users.noreply.github.com>
2021-10-01 10:16:17 +0530

    Add files via upload

---
+++ Product.java
@@ -0,0 +1,39 @@
+package com.jdbcexample;
+public class Product {
+	private Integer qty,id,price;
+	private String name;
+	private Double bill;
+	public Integer getQty() {
+		return qty;
+	}
+	public void setQty(Integer qty) {
+		this.qty = qty;
+	}
+	public Integer getPrice() {
+		return price;
+	}
+	public void setPrice(Integer price) {
+		this.price = price;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public Double getBill() {
+		return bill;
+	}
+	public void setBill(Double bill) {
+		this.bill = bill;
+	}
+	
+	
+
+}
